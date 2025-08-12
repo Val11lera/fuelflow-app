c2da15b049bcf9888c7b40e0783c4623375c19a4
Val11lera <fuelflow.queries@gmail.com>
2025-08-12 21:27:21 +0100

    Ensure src/pages/api is used for API routes

--- src/pages/api/ping.ts
+++ src/pages/api/ping.ts
@@ -1,5 +1,4 @@
-// src/pages/api/ping.ts
-import type { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from 'next';
 export default function handler(_req: NextApiRequest, res: NextApiResponse) {
   res.status(200).json({ ok: true });
 }
