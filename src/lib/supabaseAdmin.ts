b6ceef2d03e7eff989a53b4dadf022d54f952545
Val11lera <fuelflow.queries@gmail.com>
2025-08-31 14:56:37 +0100

    Update supabaseAdmin.ts

--- src/lib/supabaseAdmin.ts
+++ src/lib/supabaseAdmin.ts
@@ -1,10 +1,11 @@
 // src/lib/supabaseAdmin.ts
+// /src/lib/supabaseAdmin.ts
 import { createClient } from "@supabase/supabase-js";
 
-const url = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const key = process.env.SUPABASE_SERVICE_ROLE_KEY!; // service role (server only)
+const supabase = createClient(
+  process.env.SUPABASE_URL as string,
+  process.env.SUPABASE_SERVICE_ROLE_KEY as string,
+  { auth: { persistSession: false } }
+);
 
-const supabase = createClient(url, key, {
-  auth: { persistSession: false, autoRefreshToken: false },
-});
 export default supabase;
