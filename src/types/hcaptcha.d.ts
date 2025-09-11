9d8a854434ad3a8fb8b1ab149dd05a6da3a67f61
Val11lera <fuelflow.queries@gmail.com>
2025-09-11 15:05:00 +0100

    Update hcaptcha.d.ts

--- src/types/hcaptcha.d.ts
+++ src/types/hcaptcha.d.ts
@@ -1,7 +1,15 @@
 // src/types/hcaptcha.d.ts
+// src/types/hcaptcha.d.ts
+export {};
+
+interface HCaptchaAPI {
+  render: (el: HTMLElement, opts: any) => string | number;
+  reset: (id?: string | number) => void;
+  getResponse: (id?: string | number) => string;
+}
+
 declare global {
   interface Window {
-    hcaptcha?: any;
+    hcaptcha?: HCaptchaAPI;
   }
 }
-export {};
