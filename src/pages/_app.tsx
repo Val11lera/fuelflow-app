c82ced9d9709a4a08b92cb62817964f7bdffe471
Val11lera <fuelflow.queries@gmail.com>
2025-09-12 19:30:15 +0100

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import "@/styles/globals.css"; // the ONLY global CSS import
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
