79d652a4041a3055d7f8d6584469821afb36068e
Val11lera <fuelflow.queries@gmail.com>
2025-09-12 17:28:54 +0100

    Update supabaseAdmin.ts

--- src/lib/supabaseAdmin.ts
+++ src/lib/supabaseAdmin.ts
@@ -1,13 +1,13 @@
 // src/lib/supabaseAdmin.ts
 // src/lib/supabaseAdmin.ts
+import "server-only";
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseAdmin = createClient(
-  process.env.SUPABASE_URL as string,
+  process.env.NEXT_PUBLIC_SUPABASE_URL as string,
   process.env.SUPABASE_SERVICE_ROLE_KEY as string,
-  {
-    auth: { persistSession: false },
-  }
+  { auth: { persistSession: false } }
 );
 
+export { supabaseAdmin };
 export default supabaseAdmin;
