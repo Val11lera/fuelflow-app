ca82abb00b68c41b21c004f70ff0283c8b09ab5c
Val11lera <fuelflow.queries@gmail.com>
2025-09-12 19:34:36 +0100

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,8 @@
+// src/pages/_app.tsx
 import type { AppProps } from "next/app";
-import "@/styles/globals.css"; // the ONLY global CSS import
+
+// The ONLY place where global CSS is imported on Pages Router
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
